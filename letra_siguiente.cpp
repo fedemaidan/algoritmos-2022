8fe2cfeb318ddcf3f5092ec097f6f874428fdf8f
Federico <fede.maidan@gmail.com>
2022-04-21 09:47:07 -0300

    Letra siguiente

---
+++ letra_siguiente.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    /* Código asci */
+    char letra = 'a';
+
+    cout << "Probando siguiente letra" << endl;
+    letra++;
+    cout << "La letra siguiente es: " << letra << endl;
+
+    /*
+        Probando siguiente letra
+        La letra siguiente es: ???
+    */
+    return 0;
+}
